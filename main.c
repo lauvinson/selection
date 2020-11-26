14fa7aa631f3f6f397777074e178ab5c8113c045
vinson <liuws@zhiyun-tech.com>
2020-11-26 18:14:50 +0800

    add linux anno win

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include "server.h"
 
 int main(int argc, char *argv[]) {
-//    server_execute(argc, argv);
-    server_udp_execute(argc, argv);
+    server_execute(argc, argv);
+//    server_udp_execute(argc, argv);
 }
